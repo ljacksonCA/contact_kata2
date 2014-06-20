51a7a0415b9431d1c5940c0f953e338e4a994eee
Lonnie Jackson <ljackson@commandalkon.com>
2014-06-20 11:19:42 -0500

    Remove Contact Works and tests work

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,39 +7,62 @@ var contactControllers = angular.module('contactControllers', []);
 contactControllers.controller('ContactListCtrl', ['$scope', 'storageService',
     function($scope, storageService) {
         var data = [];
-        //var counter = storageService.get('idCounter');
-        var counter = localStorage.length -1;
-
-        for (var i =0; i < counter; i++) {
-            data.push(storageService.get(i));
+        if (storageService.has('contactList')) {
+            data = storageService.get('contactList');
         }
 
         $scope.contacts = data;
     }]
 );
 
-contactControllers.controller('ContactDetailCtrl', ['$scope', '$routeParams', 'storageService',
-    function($scope, $routeParams, storageService) {
-        $scope.contact = storageService.get($routeParams.contactId);
+contactControllers.controller('ContactDetailCtrl', ['$scope', '$routeParams', 'storageService', '$window', '$log',
+    function($scope, $routeParams, storageService, $window, $log) {
+        var data = storageService.get('contactList');
+        var contactIndex = -1;
+
+        //Find contact in array to show that contact's details
+        for (var i = 0; i < data.length; i++) {
+            if (data[i].id == $routeParams.contactId) {
+                $scope.contact = data[i];
+                contactIndex = i;
+            }
+        }
+
+        $scope.removeContact = function() {
+            var confirmation = confirm("Are you sure you want to delete " + $scope.contact.name + "?");
+
+            if (confirmation) {
+                if (contactIndex >= 0) {
+                    data.splice(contactIndex, 1);
+                    storageService.set('contactList', data);
+                }
+                $window.location.href = '#/contacts/';
+            }
+        };
     }]
 );
 
 contactControllers.controller('AddPersonCtrl', ['$scope', 'storageService', '$window',
     function($scope, storageService, $window) {
         $scope.addContact = function() {
+            if (!storageService.has('contactList')) {
+                storageService.set('contactList', []);
+            }
+
             if (!storageService.has('idCounter')) {
                 storageService.set('idCounter', 0);
             }
 
+            var info = storageService.get('contactList');
             var id = storageService.get('idCounter');
-            var info = {
+            info.push({
                 "name": $scope.nameBox,
                 "id": id,
                 "phone": $scope.phoneBox,
                 "address": $scope.addressBox
-            };
+            });
 
-            storageService.set(id, info);
+            storageService.set('contactList', info);
             storageService.set('idCounter', id + 1);
             $window.location.href = '#/contacts/';
         };
@@ -48,23 +71,31 @@ contactControllers.controller('AddPersonCtrl', ['$scope', 'storageService', '$wi
 
 contactControllers.controller('EditContactCtrl', ['$scope', '$routeParams', 'storageService', '$window',
     function($scope, $routeParams, storageService, $window) {
+        var data = storageService.get('contactList');
+        var contactIndex = -1;
 
-        $scope.contact = storageService.get($routeParams.contactId);
+        //Find contact in array to show that contact's details for editing
+        for (var i = 0; i < data.length; i++) {
+            if (data[i].id == $routeParams.contactId) {
+                $scope.contact = data[i];
+                contactIndex = i;
+            }
+        }
+
         $scope.nameBox = $scope.contact.name;
         $scope.phoneBox = $scope.contact.phone;
         $scope.addressBox = $scope.contact.address;
 
         $scope.updateContact = function() {
-            var id = $routeParams.contactId;
-            var info = {
+            data[contactIndex] = {
                 "name": $scope.nameBox,
-                "id": id,
+                "id": $routeParams.contactId,
                 "phone": $scope.phoneBox,
                 "address": $scope.addressBox
             };
 
-            storageService.set(id, info);
-            $window.location.href = '#/contacts/' + id;
+            storageService.set('contactList', data);
+            $window.location.href = '#/contacts/' + $routeParams.contactId;
         };
     }]
 );
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -5,16 +5,28 @@
 describe('controllers', function(){
 
     describe('ContactListCtrl', function() {
-        var scope, ctrl;
+        var scope, ctrl, data;
 
         beforeEach(module('contactControllers', 'contactServices'));
 
         beforeEach(inject(function($controller, storageService) {
             scope = {};
-            storageService.set('idCounter',3);
-            storageService.set('1',1);
-            storageService.set('2',1);
-            storageService.set('3',1);
+            data = [
+                {
+                    "name": "Lonnie Jackson",
+                    "id": 1,
+                    "phone": "(913)-384-0880",
+                    "address": "200 1st St."
+                },
+                {
+                    "name": "Mark Scannell",
+                    "id": 2,
+                    "phone": "(913)-384-2645",
+                    "address": "300 2nd St."
+                }
+            ];
+            storageService.set('contactList', data);
+            storageService.set('idCounter',2);
             ctrl = $controller('ContactListCtrl', { $scope:scope, storageService:storageService });
         }));
 
@@ -23,24 +35,62 @@ describe('controllers', function(){
             expect(ctrl).toBeDefined();
         });
 
-        it('should have 3 contacts', function() {
-            expect(scope.contacts.length).toBe(3);
+        it('should have 2 contacts', function() {
+            expect(scope.contacts.length).toBe(2);
         });
     });
 
     describe('ContactDetailCtrl', function() {
+        var scope, ctrl, storage;
+        var rp, data;
 
         beforeEach(module('contactControllers', 'ngRoute', 'contactServices'));
 
-        it('should have a controller ContactDetailCtrl', inject(function($controller, $routeParams) {
-            //spec body
-            var myCtrl2 = $controller('ContactDetailCtrl',
+        beforeEach(inject(function($controller, storageService, $routeParams) {
+            scope = {};
+            data = [
                 {
-                    $scope: {},
-                    $routeParams: $routeParams
-                });
-            expect(myCtrl2).toBeDefined();
+                    "name": "Lonnie Jackson",
+                    "id": 1,
+                    "phone": "(913)-384-0880",
+                    "address": "200 1st St."
+                },
+                {
+                    "name": "Mark Scannell",
+                    "id": 2,
+                    "phone": "(913)-384-2645",
+                    "address": "300 2nd St."
+                },
+                {
+                    "name": "Kyle Fisher",
+                    "id": 3,
+                    "phone": "(913)-384-2645",
+                    "address": "300 2nd St."
+                }
+            ];
+            storage = storageService;
+            storage.set('contactList', data);
+            rp = {
+                "contactId": 2
+            };
+            ctrl = $controller('ContactDetailCtrl', {
+                                    $scope:scope,
+                                    $routeParams: rp,
+                                    storageService:storage
+                                });
         }));
+
+        it('should have a controller ContactDetailCtrl', function() {
+            expect(ctrl).toBeDefined();
+        });
+
+        it('should remove contact when clicking delete button', function() {
+            expect(storage.has('contactList')).toBe(true);
+            expect(storage.get('contactList').length).toBe(3);
+            console.log(storage);
+            scope.removeContact();
+            expect(storage.get('contactList').length).toBe(2);
+        });
     });
 
 });
